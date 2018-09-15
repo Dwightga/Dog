e4f4f530c5f0e49ef572034577c588c89b6c3849
Dwight Allen <42547273+Dwightga@users.noreply.github.com>
2018-09-14 21:26:55 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Dog
+{
+    class MainClass
+    {   
+        
+        public static void Main()
+        {
+              public class Dog
+        {
+            public name(string x);
+            public owner(string y);
+            public age(int z);
+            public enum Gender
+            {
+                Male,
+                Female,
+            }
+
+           public void bark(int B)
+                {
+                B = woof;
+                Console.WriteLine("Woof!");
+                }
+
+
+                public void getTag
+                {
+                    Console.WriteLine("If lost, call {0}. {0} name is {0} and {0} is {0} years old.",owner, , name,,age  );
+                }
+
+
+
+    }
+}
+    }
+}
